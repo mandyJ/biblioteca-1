58050bf3e3d39357dedbbfb05bfddb0d957ed032
lukebelliveau <lbellive@thoughtworks.com>
2016-06-17 17:20:25 -0500

    deleting application!!!!!

--- src/test/java/com/thoughtworks/biblioteca/ApplicationTest.java
+++ src/test/java/com/thoughtworks/biblioteca/ApplicationTest.java
@@ -3,68 +3,67 @@ package com.thoughtworks.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
 
 import static org.mockito.Matchers.contains;
 import static org.mockito.Mockito.*;
 
 public class ApplicationTest {
     PrintStream printStream;
-    ArrayList<Book> books;
-    Book book;
     Application application;
-    BufferedReader bufferedReader;
+    MainMenu mainMenu;
+    Library library;
 
-
-    @Before
-    public void setUp() throws Exception {
-        printStream = mock(PrintStream.class);
-        bufferedReader = mock(BufferedReader.class);
-        books = new ArrayList<Book>();
-        book = mock(Book.class);
-        books.add(book);
-        application = new Application(books, printStream, bufferedReader);
-    }
-
-    @Test
-    public void shouldWelcomeUserWhenStarting() {
-        // Arrange OR Given
-        // Action OR When
-        application.start();
-        // Assert OR Then
-        verify(printStream).println(contains("Welcome"));
-    }
-
-
-    @Test
-    public void shouldDisplayListBooksOptionAfterWelcome() {
-        application.start();
-
-        verify(printStream).println(contains("Enter an option:"));
-        verify(printStream).println(contains("1: List Books"));
-    }
-
-    @Test
-    public void shouldNotPrintBookListBeforeUserChoosesOption() throws Exception {
-        when(bufferedReader.readLine()).thenReturn("");
-
-        application.start();
-
-        verify(book, never()).printBookDetails();
-    }
-
-    @Test
-    public void shouldPrintBookDetailsWhenUserRequestsList() throws Exception{
-        when(bufferedReader.readLine()).thenReturn("1");
-
-        application.start();
-
-        verify(book).printBookDetails();
-    }
-    
-
+//    @Before
+//    public void setUp() throws Exception {
+//        printStream = mock(PrintStream.class);
+//        mainMenu = mock(MainMenu.class);
+//        library = mock(Library.class);
+//        application = new Application(library, mainMenu, printStream);
+//
+//    }
+//
+//    @Test
+//    public void shouldWelcomeUserWhenStarting() {
+//        // Arrange OR Given
+//        // Action OR When
+//        application.runApplication();
+//        // Assert OR Then
+//        verify(printStream).println(contains("Welcome"));
+//    }
+//
+//    @Test
+//    public void shouldShowMenuOptionsWhenStart() {
+//        application.runApplication();
+//
+//        verify(mainMenu).showMenuOptions();
+//    }
+//
+//    @Test
+//    public void shouldRetrieveUserOptionWhenStart() throws IOException {
+//        application.runApplication();
+//
+//        verify(mainMenu).readUserInput();
+//    }
+//
+//    @Test
+//    public void shouldDisplayListofBooksWhenUserEntersOne() {
+//        when(mainMenu.readUserInput()).thenReturn("1");
+//
+//        application.runApplication();
+//
+//        verify(library).listBooks();
+//
+//    }
+//
+//    @Test
+//    public void shouldRepromptUserWhenInvalidOptionSelected() {
+//        when(mainMenu.readUserInput()).thenReturn("0");
+//
+//        application.runApplication();
+//
+//        verify(mainMenu).informUserOptionInvalid();
+//    }
 
 }
